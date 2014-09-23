3b85685332876d2bb247d0db7363bc9ffaa4e1d0
Alex Birdsall <ambirdsall@gmail.com>
2014-09-22 17:04:29 -0700

    Adds Cell constructor.

--- solver.js
+++ solver.js
@@ -1,5 +1,22 @@
 Game = function(board) {
 	this.board = board;
+
+Cell = function(digit) {
+	this.digit = digit;
+}
+
+Cell.prototype = {
+	isEmpty: function() {
+		return (this.digit === 0 ? true : false);
+	},
+
+	rowNum: function() {
+		return this.digit / 9;
+	},
+	
+	colNum: function() {
+		return this.digit % 9;
+	}
 }
 
 testGame = new Game("096040001100060004504810390007950043030080000405023018010630059059070830003590007");
