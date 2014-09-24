966483b5ff50518abcdebda208e11ab348848deb
Alex Birdsall <ambirdsall@gmail.com>
2014-09-23 20:10:12 -0700

    Reindents.

--- solver.js
+++ solver.js
@@ -1,53 +1,53 @@
 Game = function(boardString) {
-	this.boardSize = 81;
-	try {
-		this.makeBoard(boardString);
-	} catch(e) {
-		console.log(e)
-	}
+  this.boardSize = 81;
+  try {
+    this.makeBoard(boardString);
+  } catch(e) {
+    console.log(e)
+  }
 }
 
 Game.prototype = {
-	makeBoard: function(boardString) {
-		if (boardString.length !== this.boardSize) {
-			throw "Invalid board string";
-		}
+  makeBoard: function(boardString) {
+    if (boardString.length !== this.boardSize) {
+      throw "Invalid board string";
+    }
 
-		this.board = [];
+    this.board = [];
 
-		for (var i=0; i<this.boardSize; i++) {
-			this.board.push(new Cell(boardString.charAt(i)))
-		}
-	},
-	isSolved: function() {
-		var emptyCellsFound = false,
-		    i = 0;
-		while (!emptyCellsFound && i<this.boardSize) {
-			if (this.board[i].digit === "0") {
-				return emptyCellsFound;
-			}
-			i++;
-		}
-		return !emptyCellsFound;
-	}
+    for (var i=0; i<this.boardSize; i++) {
+      this.board.push(new Cell(boardString.charAt(i)))
+    }
+  },
+  isSolved: function() {
+    var emptyCellsFound = false,
+    i = 0;
+    while (!emptyCellsFound && i<this.boardSize) {
+      if (this.board[i].digit === "0") {
+        return emptyCellsFound;
+      }
+      i++;
+    }
+    return !emptyCellsFound;
+  }
 }
 
 Cell = function(digit) {
-	this.digit = digit;
+  this.digit = digit;
 }
 
 Cell.prototype = {
-	isEmpty: function() {
-		return (this.digit === 0 ? true : false);
-	},
+  isEmpty: function() {
+    return (this.digit === 0 ? true : false);
+  },
 
-	rowNum: function() {
-		return this.digit / 9;
-	},
-	
-	colNum: function() {
-		return this.digit % 9;
-	}
+  rowNum: function() {
+    return this.digit / 9;
+  },
+
+  colNum: function() {
+    return this.digit % 9;
+  }
 }
 
 testGame = new Game("096040001100060004504810390007950043030080000405023018010630059059070830003590007");
